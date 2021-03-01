72fa734e3d9ed9207a05cca8b7f4342926b3f04b
Mithun Nandi <mithunnandi1993@gmail.com>
2021-03-01 16:05:18 +0530

    POM Python

---
+++ POMAutomationSauceDemo-main/Config/config.py
@@ -0,0 +1,12 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "C:\\selenium-java\\chromedriver.exe"
+  #  FIREFOX_EXECUTABLE_PATH = "C:\\Chromedriver\\firefox\\geckodriver.exe"
+
+    BASE_URL = "https://www.saucedemo.com/"
+    STANDARD_USER_NAME = "standard_user"
+    EMPTY_USER_NAME = ""
+    EMPTY_PASSWORD = ""
+    LOCKED_USER_NAME = "locked_out_user"
+    PROBLEM_USER_NAME = "problem_user"
+    PERFORMANCE_USER_NAME = "performance_glitch_user"
+    PASSWORD = "secret_sauce"
